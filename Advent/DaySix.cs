e302c6240d845bfc0d49cd8ef38957a9897b9613
Gabriel Galbavý <gabriel.galbavy9@gmail.com>
2025-12-12 06:42:22 +0100

    Started Day Six
    im too tired for this

--- Advent/DaySix.cs
+++ Advent/DaySix.cs
@@ -1,9 +1,28 @@
-﻿namespace AdventOfCode2025.Advent;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using static System.String;
+
+namespace AdventOfCode2025.Advent;
 
 static partial class MySolutions
 {
     internal static long SolveDaySix()
     {
+        var numSet = new List<(int, int, int, int, char)>();
+
+        using (StreamReader sr = new StreamReader("Src/DaySix.txt"))
+        {
+            var linesInTheSand = new List<char[]>();
+            for (int i = 0; i < 4; i++)
+            {
+                linesInTheSand.Add(sr.ReadLine().ToCharArray());
+            }
+            var operations = sr.ReadLine().Split(' ');
+            foreach (var operation in operations)
+            {
+                
+            }
+        }
+
         return 0;
     }
 
