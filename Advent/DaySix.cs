69990b7957f5b7353f91c95ac250c03853866597
Gabriel Galbavý <gabriel.galbavy9@gmail.com>
2025-12-12 16:20:36 +0100

    Day Six Completed

--- Advent/DaySix.cs
+++ Advent/DaySix.cs
@@ -1,29 +1,58 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using static System.String;
+﻿using static System.String;
 
 namespace AdventOfCode2025.Advent;
 
 static partial class MySolutions
 {
-    internal static long SolveDaySix()
+    internal static Int128 SolveDaySix()
     {
-        var numSet = new List<(int, int, int, int, char)>();
+        Int128 result = 0;
+        var linesInTheSand = new List<List<string>>();
+        // last item are the operations 
 
-        using (StreamReader sr = new StreamReader("Src/DaySix.txt"))
+        using StreamReader sr = new StreamReader("Src/DaySix.txt");
+
+        for (var i = 0; i < 5; i++)
         {
-            var linesInTheSand = new List<char[]>();
-            for (int i = 0; i < 4; i++)
+            linesInTheSand.Add(
+                sr
+                .ReadLine()?
+                .Split(" ")
+                .ToList() ?? throw new InvalidOperationException());
+
+            linesInTheSand[i].RemoveAll(x => x == Empty);
+        }
+
+        for (var i = 0; i < linesInTheSand[0].Count; i++)
+        {
+            var numbers = (
+                long.Parse(linesInTheSand[0][i]), // if you use Int here the 
+                long.Parse(linesInTheSand[1][i]), // value will exceed the limit
+                long.Parse(linesInTheSand[2][i]),
+                long.Parse(linesInTheSand[3][i])
+            );
+
+            switch (linesInTheSand[4][i])
             {
-                linesInTheSand.Add(sr.ReadLine().ToCharArray());
-            }
-            var operations = sr.ReadLine().Split(' ');
-            foreach (var operation in operations)
-            {
-                
+                case "+":
+                    result +=
+                        numbers.Item1
+                        + numbers.Item2
+                        + numbers.Item3
+                        + numbers.Item4;
+                    break;
+
+                case "*":
+                    result +=
+                        numbers.Item1
+                        * numbers.Item2
+                        * numbers.Item3
+                        * numbers.Item4;
+                    break;
             }
         }
 
-        return 0;
+        return result;
     }
 
     internal static long SolveDaySixPlus()
